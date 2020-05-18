3e16af99df4caf5c20e653a2f945a7b2b5562e5d
Marwan Bataineh <batainem96@gmail.com>
2020-05-18 17:21:44 -0500

    initial commit

---
+++ provider/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+
+const port = 3001;
+
+let i = 0;
+
+app.get('/tick', (request, response) => {
+    i += 1;
+    response.send({number: i});
+});
+
+app.listen(port, () => {
+    console.log(`live on port ${port}!`);
+});
